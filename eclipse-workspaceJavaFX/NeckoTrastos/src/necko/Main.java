56a539bfcb132336b2221fca4da66e5dc6b6bbd2
potrijo <backinmarc@gmail.com>
2021-05-19 18:34:34 +0200

    Segunda prueba verificación funcionamiento

--- eclipse-workspaceJavaFX/NeckoTrastos/src/necko/Main.java
+++ eclipse-workspaceJavaFX/NeckoTrastos/src/necko/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.layout.BorderPane;
 
 public class Main extends Application {
 	@Override
+	// comentario
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
